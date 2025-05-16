f72cbf7b8897ae3f3cba13c38b8a8ac0b8117dca
tifanny-galdea <tifannygm13@gmail.com>
2025-05-16 07:51:50 -0500

    Configuracion para Render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 import app from './app.js'
 import { port } from './config.js'
 
-app.listen(port); //puerto 3000
+app.listen(process.env.PORT || 3001); //asignar un puerto del Render o el predeterminado aquí
 
 console.log('El servidor está escuchando por el puerto: ', port)
