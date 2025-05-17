4cdae127e47b9a39a97f58e3a67dc0485d8b6929
tifanny-galdea <tifannygm13@gmail.com>
2025-05-17 00:09:21 -0500

    API con Render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
 import app from './app.js'
-import { port } from './config.js'
 
 app.listen(port); //puerto 3000
 
-console.log('El servidor está escuchando por el puerto: ', port)
+console.log('Servidor Activo')
