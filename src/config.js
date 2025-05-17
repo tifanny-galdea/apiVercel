9403f2e4ebab3abfb139ebb62ba2b81dda5b1db4
tifanny-galdea <tifannygm13@gmail.com>
2025-05-17 00:02:56 -0500

    Actualizado 6

--- src/config.js
+++ src/config.js
@@ -5,5 +5,4 @@ export const bd_host = process.env.bd_host || 'dpg-d0jum3juibrs73928q7g-a'
 export const bd_database = process.env.bd_database || 'base2025_database'
 export const bd_user = process.env.bd_user || 'root'
 export const bd_password = process.env.bd_password || 'v6nO44ibkailRFq2p5tgao0MKl4MOOY9'
-export const bd_port = process.env.bd_port || 5432
-export const port = process.env.PORT || 3001
+export const bd_port = process.env.bd_port || 3306
