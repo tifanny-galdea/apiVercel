a195e12c71ac2d99d5abde42da12b65423f0fdd7
tifanny-galdea <tifannygm13@gmail.com>
2025-05-17 09:48:44 -0500

    API -Vercel-Actualizado2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 
+//presentar en mi URL inicial
+app.get('/', (req, res) => {
+  res.send('Servidor API activo y funcionando');
+});
+
 
 //subir imagenes
 import path from 'path'
